3ece612a687e2bd5ca28d433f4de7acfcb49a122
cormacwit <cormackfarell@gmail.com>
2022-12-12 12:59:40 +0000

    Full review page.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
 import {Link} from 'react-router-dom'
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         </Routes>
       </BrowserRouter>
     );
