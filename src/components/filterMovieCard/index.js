6d70e8d30e42a6ae40065497d909dea3e2a854e1
cormacwit <cormackfarell@gmail.com>
2022-12-12 01:38:21 +0000

    Added routing and live data from API.

--- src/components/filterMovieCard/index.js
+++ src/components/filterMovieCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -19,60 +19,87 @@ const formControl =
   };
 
 export default function FilterMoviesCard(props) {
+    const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
-  const genres = [
-    {id: 1, name: "Animation"},
-    {id: 2, name: "Comedy"},
-    {id: 3, name: "Thriller"}
-  ]
+    useEffect(() => {
+      fetch(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        process.env.REACT_APP_TMDB_KEY
+      )
+        .then(res => res.json())
+        .then(json => {
+          // console.log(json.genres) 
+          return json.genres
+        })
+        .then(apiGenres => {
+          setGenres([genres[0], ...apiGenres]);
+        });
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return (
-    <Card 
-      sx={{
-        maxWidth: 345,
-        backgroundColor: "rgb(204, 204, 0)"
-      }} 
-      variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-        </Typography>
-        <TextField
-          sx={formControl}
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <FormControl sx={formControl}>
-          <InputLabel id="genre-label">Genre</InputLabel>
-          <Select
-            labelId="genre-label"
-            id="genre-select"
-          >
-            {genres.map((genre) => {
-              return (
-                <MenuItem key={genre.id} value={genre.id}>
-                  {genre.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
-        </FormControl>
-      </CardContent>
-      <CardMedia
-        sx={{ height: 300 }}
-        image={img}
-        title="Filter"
-      />
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-          <br />
-        </Typography>
-      </CardContent>
-    </Card>
-  );
-}
+    const handleChange = (e, type, value) => {
+        e.preventDefault()
+        // Completed later
+      };
+      const handleTextChange = e => {
+        handleChange(e, "name", e.target.value)
+      }
+      const handleGenreChange = e => {
+        handleChange(e, "genre", e.target.value)
+      };
+
+      return (
+        <Card 
+          sx={{
+            maxWidth: 345,
+            backgroundColor: "rgb(204, 204, 0)"
+          }} 
+          variant="outlined">
+          <CardContent>
+            <Typography variant="h5" component="h1">
+              <SearchIcon fontSize="large" />
+              Filter the movies.
+            </Typography>
+            <TextField
+      sx={formControl}
+      id="filled-search"
+      label="Search field"
+      type="search"
+      variant="filled"
+      value={props.titleFilter}
+      onChange={handleTextChange}
+    />
+            <FormControl sx={formControl}>
+              <InputLabel id="genre-label">Genre</InputLabel>
+              <Select
+    labelId="genre-label"
+    id="genre-select"
+    defaultValue=""
+    value={props.genreFilter}
+    onChange={handleGenreChange}
+  >
+                {genres.map((genre) => {
+                  return (
+                    <MenuItem key={genre.id} value={genre.id}>
+                      {genre.name}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </CardContent>
+          <CardMedia
+            sx={{ height: 300 }}
+            image={img}
+            title="Filter"
+          />
+          <CardContent>
+            <Typography variant="h5" component="h1">
+              <SearchIcon fontSize="large" />
+              Filter the movies.
+              <br />
+            </Typography>
+          </CardContent>
+        </Card>
+      );
+    }
